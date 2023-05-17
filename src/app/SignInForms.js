b4a454d3ea774d718b298c278d4192974cf1c092
Andrés Monsivais Salazar <92643505+parlack@users.noreply.github.com>
2023-05-16 20:59:48 -0600

    UPDATE
    update casi listo para entregar, no es una version completamente finalizada, ya que carece de muchas cosas, po rejemplo, el flex

--- src/app/SignInForms.js
+++ src/app/SignInForms.js
@@ -1,94 +1,88 @@
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js"
-import { auth } from "./firebaseconfig.js"
-import {getDocs,addDoc} from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js';
-import { colRef } from "./firebaseconfig.js"
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
+import { auth } from "./firebaseconfig.js";
+import {
+  getDocs,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
+import { colRef } from "./firebaseconfig.js";
 
 const signinForm = document.querySelector("#formlogin");
 
-signinForm.addEventListener('submit', async (e) => {
-    e.preventDefault()
+signinForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    const email = signinForm['lo_email_input'].value;
-    const password= signinForm['lo_pass_input'].value;
+  const email = signinForm["lo_email_input"].value;
+  const password = signinForm["lo_pass_input"].value;
 
+  try {
+    const usercredentials = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    //console.log(usercredentials)
 
+    console.log();
 
-    try{
-    const usercredentials = await signInWithEmailAndPassword(auth, email, password)
-        //console.log(usercredentials)
+    getidforemail(email)
+      .then((ids) => {
+        localStorage.setItem("id", ids);
 
-        console.log();
+        localStorage.setItem("loged", true);
 
-        getidforemail(email)
-  .then((ids) => {
-    console.log(ids); // muestra el valor de ids
-    window.location.href="./HealistPa.html"+"?"+"id="+ids
+        window.location.href = "./HealistPa.html";
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } catch (error) {
+    console.log(error);
 
-  })
-  .catch((error) => {
-    console.error(error); // maneja cualquier error que ocurra en la promesa
-  });
-
-
-
-    }catch(error){
-        console.log(error)
-
-        if(error.code==='auth/user-not-found'||error.code==='auth/wrong-password' ||error.code=== 'auth/too-many-requests'){
-
-            texterror.textContent="*The password or Email is incorrect"; 
-
-        }else{
-            texterror.textContent="*Error, please contact admin";  
-        }
-        
-
-
+    if (
+      error.code === "auth/user-not-found" ||
+      error.code === "auth/wrong-password" ||
+      error.code === "auth/too-many-requests"
+    ) {
+      texterror.textContent = "*The password or Email is incorrect";
+    } else {
+      texterror.textContent = "*Error, please contact admin";
     }
-
-})
-
+  }
+});
 
 function getidforemail(userem) {
-    return new Promise((resolve, reject) => {
-      getDocs(colRef)
-        .then((snapshot) => {
-          let users = [];
-          snapshot.docs.forEach((doc) => {
-            users.push({ ...doc.data(), id: doc.id });
-          });
-  
-          function getEmailIds(json) {
-            const emailIds = [];
-            json.forEach((obj) => {
-              if (obj.hasOwnProperty("email")) {
-                emailIds.push({ email: obj.email, id: obj.id });
-              }
-            });
-            return emailIds;
-          }
-  
-          const emailIds = getEmailIds(users);
-  
-          let ids = "";
-          for (let i = 0; i < emailIds.length; i++) {
-            if (emailIds[i].email === userem) {
-              ids = emailIds[i].id;
-              break;
-            }
-          }
-  
-          resolve(ids); // resuelve la promesa con el valor de ids
-        })
-        .catch((error) => {
-          reject(error); // rechaza la promesa si hay un error en la consulta
+  return new Promise((resolve, reject) => {
+    getDocs(colRef)
+      .then((snapshot) => {
+        let users = [];
+        snapshot.docs.forEach((doc) => {
+          users.push({ ...doc.data(), id: doc.id });
         });
-    });
-  }
-   
-    
 
+        function getEmailIds(json) {
+          const emailIds = [];
+          json.forEach((obj) => {
+            if (obj.hasOwnProperty("email")) {
+              emailIds.push({ email: obj.email, id: obj.id });
+            }
+          });
+          return emailIds;
+        }
 
+        const emailIds = getEmailIds(users);
 
+        let ids = "";
+        for (let i = 0; i < emailIds.length; i++) {
+          if (emailIds[i].email === userem) {
+            ids = emailIds[i].id;
+            break;
+          }
+        }
 
-
+        resolve(ids);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
--- src/app/firebaseconfig.js
+++ src/app/firebaseconfig.js
@@ -1,33 +1,35 @@
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 
-    import { getAuth } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 
-    import {getFirestore,collection, getDocs} from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
 
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-  
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-      apiKey: "AIzaSyCvIdtEigp8ZS_o9IkK469ZlqPxuuvI-1Q",
-      authDomain: "healist-467d2.firebaseapp.com",
-      projectId: "healist-467d2",
-      storageBucket: "healist-467d2.appspot.com",
-      messagingSenderId: "1080277926926",
-      appId: "1:1080277926926:web:d939302e39eefe2c87e85f",
-      measurementId: "G-0Z44T86TJ4"
-    };
-  
-    // Initialize Firebase
-    export const auth = getAuth(initializeApp(firebaseConfig));
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    initializeApp(firebaseConfig);
-    // init services
-     const db = getFirestore();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvIdtEigp8ZS_o9IkK469ZlqPxuuvI-1Q",
+  authDomain: "healist-467d2.firebaseapp.com",
+  projectId: "healist-467d2",
+  storageBucket: "healist-467d2.appspot.com",
+  messagingSenderId: "1080277926926",
+  appId: "1:1080277926926:web:d939302e39eefe2c87e85f",
+  measurementId: "G-0Z44T86TJ4",
+};
 
-    //collection ref
-    export const colRef = collection(db, 'users');
+// Initialize Firebase
+export const auth = getAuth(initializeApp(firebaseConfig));
 
+initializeApp(firebaseConfig);
+// init services
+const db = getFirestore();
 
+//collection ref
+export const colRef = collection(db, "users");
